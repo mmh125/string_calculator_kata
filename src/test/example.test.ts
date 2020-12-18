bca7f72aba726c8e83a841b0fc26a2f0b23147ea
muna.hussein <muna.hussein@cazoo.co.uk>
2020-12-18 15:42:25 +0000

    big blob

---
+++ src/test/example.test.ts
@@ -0,0 +1,26 @@
+describe("string_calc", () => {
+    it("should return the sum of inputs", () => {
+        expect(string_calc("")).toBe(0);
+    });
+    it("should return 1 for 1", () => {
+        expect(string_calc("1")).toBe(1);
+    });
+    it("should return 3 for 1,2", () => {
+        expect(string_calc("1,2")).toBe(3);
+    });
+});
+​
+function string_calc(s: string) {
+    if (s === "") return 0
+​
+    let bits = s.split(',');
+​
+    let sum = 0;
+    for (const b of bits) {
+        sum += parseInt(b);
+    }
+​
+    return sum;
+}
+
+
